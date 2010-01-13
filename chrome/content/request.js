81b23d900a21e2d2618244fd53b438c428f6f3e8
Moritz Reinhardt <iaminaustralia@gmail.com>
2010-01-13 09:54:04 +0100

    added request to display only my lvs with lecturer + mark

--- chrome/content/request.js
+++ chrome/content/request.js
@@ -22,6 +22,8 @@ var piswirequest = {
 //HIER beginnts!!
     var xmlHttpObject = new XMLHttpRequest();
 
+    var kennwort = "KENNWORT"; //TODO
+    var matnr = "0804616";
     // Funktion, die bei Statusänderungen reagiert
     function handleStateChange()
     {
@@ -44,40 +46,54 @@ var piswirequest = {
 //			xmlHttpObject.setRequestHeader('Referer','http://sibelius.pri.univie.ac.at:8885/piswi/xxx/piswi.x');
 			xmlHttpObject.onreadystatechange = function (aEvt) {  
 				if (xmlHttpObject.readyState == 4) {  
-					if(xmlHttpObject.status == 200) {
-						//alert(xmlHttpObject.responseText);
-						infoReceived();
+					if(xmlHttpObject.status == 200) { //logged in
+						xmlHttpObject.open('POST', 'http://sibelius.pri.univie.ac.at:8885/piswi/xxx/piswi.x');
+						xmlHttpObject.setRequestHeader('Connection','keep-alive');
+						xmlHttpObject.setRequestHeader('Referer','http://sibelius.pri.univie.ac.at:8885/piswi/xxx/piswi.x');
+						xmlHttpObject.setRequestHeader('imnr',matnr);
+						xmlHttpObject.setRequestHeader('ikenn',kennwort);
+						xmlHttpObject.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+						xmlHttpObject.setRequestHeader('Content-Length', 94);
+						xmlHttpObject.onreadystatechange = function (bEvt) {
+							if (xmlHttpObject.readyState == 4) {
+								if(xmlHttpObject.status == 200) { //displaying my lvs with description + mark
+									infoReceived();
+								} else 
+									alert("Error loading page\n");
+							}
+						}
+						xmlHttpObject.send("iknr=521&ipkt=0&ilv=&iaus=M&ise=2009W&iord=A&ilek=On&inot=On&imod=O&isid="+isid+"1");
+						//using DOM to modify the xul-file and insert the information
+						function infoReceived() {
+							var samplepopup = document.getElementById('showpiswires');
+							var output = xmlHttpObject.responseText;
+
+							//search string for first course
+							var tdcount = 0;
+							var position = 0;
+							do {
+								var tempposition = output.search("<TD");
+								position = position + tempposition + 3;
+								output = output.slice(tempposition + 3); //+3 so current "<TD" is skipped
+								//alert(tempposition + output);
+								//alert(tdcount + output);
+								tdcount++;
+							} while (tdcount < 75);
+							var endposition = position + output.search("</TD");
+							position = position -3; //remove first +3 because none was added in the beginning
+							var inhalt = xmlHttpObject.responseText.substring(position, endposition);
+							alert(inhalt);
+							/*var output = xmlHttpObject.responseXML;
+
+							var tds = output.getElementsByTagName("TD");
+							var first_course = tds.item(199);
+							alert(first_course.childNodes.item(0).nodeValue);*/
+						}
 					} else
 						alert("Error loading page\n");  
 				}
-			}; //TODO KENNWORT
-			xmlHttpObject.send("imnr=0804616&ikenn=KENNWORT&isid="+isid+"&imod="+imod);
-			//using DOM to modify the xul-file and insert the information
-			function infoReceived() {
-				var samplepopup = document.getElementById('showpiswires');
-				var output = xmlHttpObject.responseText;
-
-				//search string for first course
-				var tdcount = 0;
-				var position = 0;
-				do {
-					var tempposition = output.search("<TD");
-					position = position + tempposition + 3;
-					output = output.slice(tempposition + 3); //+3 so current "<TD" is skipped
-					//alert(tempposition + output);
-					//alert(tdcount + output);
-					tdcount++;
-				} while (tdcount < 75);
-				var endposition = position + output.search("</TD");
-				position = position -3; //remove first +3 because none was added in the beginning
-				var inhalt = xmlHttpObject.responseText.substring(position, endposition);
-				alert(inhalt);
-				/*var output = xmlHttpObject.responseXML;
-
-				var tds = output.getElementsByTagName("TD");
-				var first_course = tds.item(199);
-				alert(first_course.childNodes.item(0).nodeValue);*/
-			}
+			};
+			xmlHttpObject.send("imnr="+matnr+"&ikenn="+kennwort+"&isid="+isid+"&imod="+imod);
 		} else alert("error loading page!");
 	}
     }
