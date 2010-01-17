0663ad9dbe8a90f161e0852f31e4fd5c0807c923
Moritz Reinhardt <iaminaustralia@gmail.com>
2010-01-17 19:14:46 +0100

    added parser - now working for LVs with description/lecturer

--- chrome/content/request.js
+++ chrome/content/request.js
@@ -67,10 +67,26 @@ var piswirequest = {
 						function infoReceived() {
 							var samplepopup = document.getElementById('showpiswires');
 							var output = xmlHttpObject.responseText;
+							var lvs = new Array();
 
-							//search string for first course
+							//count how many <TD s in response
 							var tdcount = 0;
 							var position = 0;
+							while (1) {
+								var tempposition = output.search("<TD");
+								if(tempposition < 0) break;
+								position = position + tempposition + 3;
+								output = output.slice(tempposition + 3); //+3 so current "<TD" is skipped
+								++tdcount;
+							}
+							var td_max = tdcount;  //how many <TD s in file
+							
+							//reset
+							tdcount = 0;
+							position = 0;
+							output = xmlHttpObject.responseText;
+
+							//find first lv
 							do {
 								var tempposition = output.search("<TD");
 								position = position + tempposition + 3;
@@ -79,10 +95,60 @@ var piswirequest = {
 								//alert(tdcount + output);
 								tdcount++;
 							} while (tdcount < 75);
-							var endposition = position + output.search("</TD");
-							position = position -3; //remove first +3 because none was added in the beginning
-							var inhalt = xmlHttpObject.responseText.substring(position, endposition);
-							alert(inhalt);
+
+							/* final array will look like this
+								[0] = description
+								[1] = lecturer
+								[2] = mark
+								[3] = link to smiley.gif
+							*/
+							//check if more to go (even on first)
+							for (var i=0; td_max - tdcount > 4; ++i) {
+								var current = new Array();
+								lvs.push(current); // = lvs[i]
+								var endposition = position + output.search("<") - 2; //-2 to remove \n and space
+								var description = xmlHttpObject.responseText.substring(position + 2 + 1, endposition); //move +2 because we are at <TD and skip " >" // +1 to remove \n
+								var position2 = output.search("TARGET=details>");
+								//alert(position2);
+								if(position2 == -1 || position2 > 150) { //just guessing that the next link must be further away
+									//no link, just name of lecturer
+									position2 = output.search("<I>");
+									endposition = output.search("</I>");
+									var lecturer = output.substring(position2 + 3, endposition); //+3 to skip <I>
+								} else {
+									//link for lecturer
+									endposition = output.search("</A>");
+									var lecturer = output.substring(position2 + 15, endposition); //+15 to skip TARGET=details>
+									//alert(output);
+									//alert(lecturer);
+								}
+
+								/* not working -> adjust parser before ... 
+								//trim strings (remove leading/trailing whitespaces
+								description.replace(/[\n\r]+/g,"");
+								lecturer.replace(/[\n\r]+/g,"");
+								description.replace(/^\s+|\s+$/g,"");
+								lecturer.replace(/^\s+|\s+$/g,"");*/
+
+								//add the results to the array
+								lvs[i].push(description);
+								lvs[i].push(lecturer);
+								
+								for(var k=0; k<13; ++k) {  //13 TDs further is next lv
+									var tempposition = output.search("<TD");
+									position = position + tempposition + 3;
+									output = output.slice(tempposition + 3); //+3 so current "<TD" is skipped
+									++tdcount; //TODO right number of counts?? - seems to work
+								}
+								//TODO: parse beurteilungsinfo -> mark and parse .gif
+							}
+
+							//test output
+							var ausgabe = "";
+							for (var i=0; i<lvs.length; ++i)
+								ausgabe = ausgabe + "Name: " + lvs[i][0] + "\nLecturer: " + lvs[i][1] + "\n";
+							alert(ausgabe);
+
 							/*var output = xmlHttpObject.responseXML;
 
 							var tds = output.getElementsByTagName("TD");
